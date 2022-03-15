c6d8054b7c053a35cbb19fcf0f722f2f28bdad48
paul <paulchibiukeigweze@gmail.com>
2022-03-16 00:06:11 +0100

    user account authentication

--- controllers/LoginController.js
+++ controllers/LoginController.js
@@ -1,11 +1,33 @@
 const User = require('../models/User')
 const bcrypt = require('bcryptjs')
-
+const jwt = require('jsonwebtoken')
 const { loginValidation } = require('../validation')
 
-const authenticateUser = (req, res) => {
+const authenticateUser = async (req, res) => {
 
-    res.send('Wawu we got here buddy!')
+   // validate request 
+   const {error} = loginValidation(req.body)
+   if(error) return res.json({status:400, message:error.details[0].message})
+
+   // check if user exists using email address
+   const user = await User.findOne({ email:req.body.email })
+   if(!user) return res.json({status:400, message:'Email or password is incorrect.'})
+
+   // check if password is valid
+   const validPassword = await bcrypt.compare(req.body.password, user.password)
+   if(!validPassword) return res.json({status:400, message:'Email or password is incorrect.'})
+
+
+   try {
+
+       const token = jwt.sign({id: user._id}, process.env.JWT_SECRET)
+       res.header('auth-token', token).json({status:200, _token:token, message:'User Authenticated.'})
+
+   } catch(err) {
+
+        res.json({status:400, message:'User authentication failed'})
+
+   }
 
 }
 
