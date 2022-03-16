be4aeccbe6b0fd49639f2193f20c616173eccd52
paul <paulchibiukeigweze@gmail.com>
2022-03-16 23:32:20 +0100

    transfer and withdrawal endpoints documentation

--- routes/transfer.js
+++ routes/transfer.js
@@ -5,6 +5,70 @@ const verifiedUser=require('./verifyAuth')
 const TransferController = require('../controllers/TransferController')
 
 // transfer fund 
+
+/**
+ * @swagger
+ * /api/user/transfers/create:
+ *  post:
+ *   description: Use this endpoint to transfer funds to another user account
+ *   requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            type: object
+ *            properties:
+ *              amount:
+ *                type: integer,
+ *                example: 100
+ *              beneficiary_email:
+ *                type: string
+ *                example: example@mail.com
+ *                description: Specify the user account you want to transfer funds to
+ *   responses:
+ *    '200':
+ *     description: OK
+ *     content:
+ *       application/json:
+ *         schema:
+ *           type: object
+ *           properties:
+ *              status:
+ *                type: integer
+ *                description: response status
+ *                example: 200
+ *              message:
+ *                type: string
+ *                description: response message
+ *    '400':
+ *     description: Bad Request
+ *     content:
+ *       application/json:
+ *         schema:
+ *           type: object
+ *           properties:
+ *              status:
+ *                type: integer
+ *                description: response status
+ *                example: 400
+ *              message:
+ *                type: string
+ *                description: error message
+ *    '404':
+ *      description: Not Found
+ *      content:
+ *        application/json:
+ *          schema:
+ *            type: object
+ *            properties:
+ *               status:
+ *                 type: integer
+ *               message:
+ *                 type: string
+ *                 description: error message
+ *         
+ */
+
 router.post('/create', verifiedUser, TransferController.createTransfer)
 
 module.exports = router
--- routes/withdrawal.js
+++ routes/withdrawal.js
@@ -5,6 +5,53 @@ const verifiedUser=require('./verifyAuth')
 const WithdrawalController = require('../controllers/WithdrawalController')
 
 // withdraw fund 
+
+/**
+ * @swagger
+ * /api/user/withdrawals/create:
+ *  post:
+ *   description: Use this endpoint to withdraw funds from user account
+ *   requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            type: object
+ *            properties:
+ *              amount:
+ *                type: integer,
+ *                example: 100
+ *   responses:
+ *    '200':
+ *     description: OK
+ *     content:
+ *       application/json:
+ *         schema:
+ *           type: object
+ *           properties:
+ *              status:
+ *                type: integer
+ *                description: response status
+ *                example: 200
+ *              message:
+ *                type: string
+ *                description: response message
+ *    '400':
+ *     description: Bad Request
+ *     content:
+ *       application/json:
+ *         schema:
+ *           type: object
+ *           properties:
+ *              status:
+ *                type: integer
+ *                description: response status
+ *                example: 400
+ *              message:
+ *                type: string
+ *                description: error message
+ *         
+ */
 router.post('/create', verifiedUser, WithdrawalController.createWithdrawal)
 
 module.exports = router
