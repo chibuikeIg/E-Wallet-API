c41daeb0ebef6307baa965b7d88376399cc1e3db
paul <paulchibiukeigweze@gmail.com>
2022-03-16 14:39:18 +0100

    withdrawal feature completed

--- controllers/WithdrawalController.js
+++ controllers/WithdrawalController.js
@@ -1,4 +1,3 @@
-const User = require('../models/User')
 const Transaction = require('../models/Transaction')
 const Wallet = require('../models/Wallet')
 
@@ -7,9 +6,35 @@ const {updateWallet} = require('../controllers/WalletController')
 const {randomChars} = require('../helpers')
 
 
-const createWithdrawal = (req, res) => {
+const createWithdrawal = async (req, res) => {
 
-    res.json({status:200, message:'Hello world'})
+    // validate incoming request
+    const {error} = withdrawalRequestValidation(req.body)
+    if(error) return res.json({status:400, message:error.details[0].message})
+
+    // fetch user balance
+    const wallet = await Wallet.findOne({ user_id:req.user.id })
+    if(!wallet || wallet.balance < req.body.amount) return res.json({status:400, message:'Insufficient balance'})
+
+    // update user wallet balance
+    updateWallet(req.user.id, req.body.amount, 'decrease', (error, message) => {
+
+        if(error) return res.json({status:400, message:message})
+
+        // store transaction details/ basic information
+        const transaction  = new Transaction({
+            user_id: req.user.id,
+            reference: randomChars(10),
+            amount: req.body.amount,
+            status: 'verified',
+            type:'withdrawal'
+        })
+
+        transaction.save()
+        .then((result) => res.json({status:200, message:'Successfully withdraw funds!'}))
+        .catch((err) => res.json({status:400, message:err}))
+
+    });
 }
 
 
