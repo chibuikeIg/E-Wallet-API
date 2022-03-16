b3432889cc446d16dbdf0157e4a1d5ef74a21f91
paul <paulchibiukeigweze@gmail.com>
2022-03-16 23:01:40 +0100

    api documentation

--- app.js
+++ app.js
@@ -3,12 +3,37 @@ const mongoose = require('mongoose')
 const cors = require('cors')
 require('dotenv/config')
 
+// swagger documentation autogen
+
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+
 // initialize the express application
 const app = express()
 
 // initialize db connection and // listen for incoming request
 mongoose.connect(process.env.DB_CONNECTION, {useNewUrlParser:true, useUnifiedTopology:true})
 .then((result) => app.listen(process.env.PORT)).catch((err)=> console.log(err))
+
+
+const swaggerOptions = {
+    swaggerDefinition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'Nodejs User Wallet System API',
+            description: 'Nodejs user wallet system api developer test documention',
+            contact: {
+                name: 'Paul Chibuike Igweze'
+            },
+            servers: [process.env.APP_URL]
+        }
+    },
+
+    apis: ['./routes/*.js']
+}
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs))
 
 //import routes
 const authRoute= require('./routes/auth')
@@ -17,6 +42,7 @@ app.use(cors())
 
 // parse json request
 app.use(express.json())
+
 
 // routes middleware
 app.use('/api/user', authRoute)
--- routes/auth.js
+++ routes/auth.js
@@ -5,9 +5,118 @@ const RegistrationController = require('../controllers/RegistrationController')
 const LoginController = require('../controllers/LoginController')
 
 // create user account
+
+/**
+ * @swagger
+ * /api/user/register:
+ *  post:
+ *   description: Use this endpoint to create new user account
+ *   requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            type: object
+ *            properties:
+ *              name:
+ *                type: string,
+ *                example: John Doe
+ *              email:
+ *                type: string
+ *                example: example@domain.com
+ *              password:
+ *                type: string
+ *                example: example123
+ *   responses:
+ *    '200':
+ *     description: OK
+ *     content:
+ *       application/json:
+ *         schema:
+ *           type: object
+ *           properties:
+ *              status:
+ *                type: integer
+ *                description: response status
+ *                example: 200
+ *              message:
+ *                type: string
+ *                description: response message
+ *                example: User account successfully created
+ *    '400':
+ *     description: Bad Request
+ *     content:
+ *       application/json:
+ *         schema:
+ *           type: object
+ *           properties:
+ *              status:
+ *                type: integer
+ *                description: response status
+ *                example: 400
+ *              message:
+ *                type: string
+ *                description: response message
+ *                example: Failed to create user account.
+ */
+
 router.post('/register', RegistrationController.storeUser)
 
 // authenticate user
+
+/**
+ * @swagger
+ * /api/user/login:
+ *  post:
+ *   description: Use this endpoint to create a login session for user
+ *   requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            type: object
+ *            properties:
+ *              email:
+ *                type: string
+ *                example: example@domain.com
+ *              password:
+ *                type: string
+ *                example: example123
+ *   responses:
+ *    '200':
+ *     description: OK
+ *     content:
+ *       application/json:
+ *         schema:
+ *           type: object
+ *           properties:
+ *              status:
+ *                type: integer
+ *                description: response status
+ *                example: 200
+ *              _token:
+ *                type: string
+ *                description: access token
+ *              message:
+ *                type: string
+ *                description: response message
+ *                example: User Authenticated
+ *    '400':
+ *     description: Bad Request
+ *     content:
+ *       application/json:
+ *         schema:
+ *           type: object
+ *           properties:
+ *              status:
+ *                type: integer
+ *                description: response status
+ *                example: 400
+ *              message:
+ *                type: string
+ *                description: response message
+ *                example: Failed to create user account.
+ */
 router.post('/login', LoginController.authenticateUser)
 
 // import routes
