58ff112d63ea36782c1cbbfe6990b365b3e8ede6
paul <paulchibiukeigweze@gmail.com>
2022-03-16 09:31:59 +0100

    config payment gateway

---
+++ config/paystack.js
@@ -0,0 +1,42 @@
+module.exports = (request) => {
+
+    const initializePayment = (form, mycallback) => {
+
+        const option = {
+            url : 'https://api.paystack.co/transaction/initialize',
+            headers : {
+                authorization: process.env.PAYSTACK_SECRET_KEY,
+                'content-type': 'application/json',
+                'cache-control': 'no-cache'
+           },
+           form
+        }
+
+        const callback = (error, response, body)=>{
+            return mycallback(error, body);
+        }
+
+        request.post(option, callback);
+    }
+
+    const verifyPayment = (ref,mycallback) => {
+
+        const option = {
+            url : 'https://api.paystack.co/transaction/verify/' + encodeURIComponent(ref),
+            headers : {
+                authorization: process.env.PAYSTACK_SECRET_KEY,
+                'content-type': 'application/json',
+                'cache-control': 'no-cache'
+           }
+        }
+
+        const callback = (error, response, body)=>{
+            return mycallback(error, body);
+        }
+
+
+        request(option,callback);
+    }
+
+    return {initializePayment, verifyPayment};
+}
