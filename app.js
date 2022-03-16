6e42394f88d22ed25edd7219fc2edd0e89e792ec
paul <paulchibiukeigweze@gmail.com>
2022-03-16 14:44:55 +0100

    installed cors

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express= require('express')
 const mongoose = require('mongoose')
+const cors = require('cors')
 require('dotenv/config')
 
 // initialize the express application
@@ -14,6 +15,7 @@ const authRoute= require('./routes/auth')
 
 
 // parse json request
+app.use(cors())
 app.use(express.json())
 
 // routes middleware
